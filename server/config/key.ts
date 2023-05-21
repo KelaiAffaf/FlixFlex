0222d4fd102b9969e807e19fde0047d88c8d7e9d
afaf <afaf.kelai@lastingdynamics.com>
2023-05-21 20:15:53 +0100

    setup boilerplate

---
+++ server/config/key.ts
@@ -0,0 +1,6 @@
+import prodConfig from './prod';
+import devConfig from './dev';
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+export default config;
---
+++ server/config/prod.ts
@@ -0,0 +1,6 @@
+const config = {
+    mongoURI: process.env.MONGO_URI,
+  };
+  
+  export default config;
+  
